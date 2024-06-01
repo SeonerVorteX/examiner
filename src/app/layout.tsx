3a4fe3692a577c8c5e8467a0f70160b5977c5c32
Mehdi Safarzade <seferzademehdi@gmail.com>
2024-06-01 16:26:16 +0400

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,6 +52,13 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="az">
+            <head>
+                <script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9056907287164851"
+                    crossOrigin="anonymous"
+                ></script>
+            </head>
             <body>{children}</body>
         </html>
     );
